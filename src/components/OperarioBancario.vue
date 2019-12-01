e6fcc9f518ef8ebba5bf73919203e707b0bbb543
Nicolas Arias <nicolasarias870@gmail.com>
2019-12-01 01:00:45 -0600

    picker

--- src/components/OperarioBancario.vue
+++ src/components/OperarioBancario.vue
@@ -2,6 +2,29 @@
   <v-card>
     <v-card-title>
       Hora óptima: {{displayedHour}}
+      <v-spacer></v-spacer>
+      <v-dialog
+        v-model="dateModal"
+        ref="dialog"
+        :return-value.sync="selectedDate"
+        persistent
+        width="290px"
+      >
+        <template v-slot:activator="{ on }">
+          <v-text-field
+            v-model="selectedDate"
+            label="Fecha de simulación"
+            prepend-icon="event"
+            readonly
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker v-model="selectedDate" scrollable>
+          <v-spacer></v-spacer>
+          <v-btn text color="primary" @click="dateModal = false">Cancel</v-btn>
+          <v-btn text color="primary" @click="$refs.dialog.save(selectedDate)">OK</v-btn>
+        </v-date-picker>
+      </v-dialog>
     </v-card-title>
     <div ref="operatorMap" class="mapa"></div>
   </v-card>
@@ -20,6 +43,8 @@ export default {
   },
   data() {
     return {
+      dateModal: null,
+      selectedDate: null,
       map: null,
       cajeros: null,
       ruta: null,
